f7b348639e0aa177b31705d3d025512dde68d489
Brent Gauthier <cajundrummer@gmail.com>
2025-04-21 19:37:33 -0400

    Add functionality for user to define & save preferences

---
+++ server/routes/preferences.js
@@ -0,0 +1,96 @@
+const express = require("express")
+const router = express.Router()
+const pool = require("../db")
+const auth = require("../middleware/authMiddleware")
+
+// CREATE or UPDATE preferences
+router.post('/', auth, async (req, res) => {
+    const user_id = req.user.user_id
+    const {
+        platforms,
+        brands,
+        case_size_min,
+        case_size_max,
+        strap_styles,
+        movements,
+        watch_styles,
+        price_min,
+        price_max,
+        seller_location,
+        condition,
+        dial_colors,
+        frequency
+    } = req.body
+    
+    try {
+        // Check if preferences already exist
+        const existing = await pool.query(
+            'SELECT * FROM preferences WHERE user_id = $1',
+            [user_id]
+        )
+
+        if (existing.rows.length > 0) {
+            // Update
+            await pool.query(
+                `UPDATE preferences SET
+                    platforms = $1, brands = $2, case_size_min = $3, case_size_max = $4, 
+                    strap_styles = $5, movements = $6, watch_styles = $7, 
+                    price_min = $8, price_max = $9, seller_location = $10, 
+                    condition = $11, dial_colors = $12, frequency = $13 
+                WHERE user_id = $14`,
+                [
+                    platforms, brands, case_size_min, case_size_max, 
+                    strap_styles, movements, watch_styles, 
+                    price_min, price_max, seller_location, condition, dial_colors, frequency, 
+                    user_id
+                ]
+            )
+
+            return res.status(200).json({message: "Preferences updated"})
+        } 
+
+        else {
+            // Insert new
+            await pool.query(
+                `INSERT INTO preferences (
+                user_id, platforms, brands, case_size_min, case_size_max, 
+                strap_styles, movements, watch_styles, 
+                price_min, price_max, seller_location, 
+                condition, dial_colors, frequency
+                ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`,
+                 [user_id, platforms, brands, case_size_min, case_size_max, 
+                    strap_styles, movements, watch_styles,
+                    price_min, price_max, seller_location,  
+                    condition, dial_colors, frequency
+                 ]
+            )
+            return res.status(201).json({message: "Preferences saved"})
+        }
+    } catch (err) {
+        console.error("Preferences error: ", err.message)
+        res.status(500).json({error: "Failed to safe preferences"})
+    }
+})
+
+// GET preferences for current user
+router.get("/", auth, async (req, res) => {
+    const user_id = req.user.user_id
+
+    try {
+        const prefs = await pool.query(
+            'SELECT * FROM preferences WHERE user_id = $1',
+            [user_id]
+        )
+
+        if (prefs.rows.length === 0) {
+            return res.status(404).json({error: "No preferences found"})
+        }
+
+        return res.status(200).json(prefs.rows[0])
+    } catch (err) {
+        console.error("Get preferences error: ", err.message)
+        res.status(200).json({error: "Failed to get preferences"})
+    }
+})
+
+module.exports = router
