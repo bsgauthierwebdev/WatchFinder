791ddf45290ef510571f3531f4879af40dc9ae33
Brent Gauthier <cajundrummer@gmail.com>
2025-05-22 22:57:15 -0400

    Update CORS options to allow for Vite's standard port

--- server/server.js
+++ server/server.js
@@ -4,17 +4,24 @@ const cors = require('cors')
 require('dotenv').config()
 const path = require("path")
 const securityMiddleware = require("./middleware/securityMiddleware")
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"]
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(helmet())
-const corsOptions = {
-    origin: process.env.CLIENT_URL || "http://localhost:3000", // Allow only my front-end
-    methods: ["GET", "POST", "PUT", "DELETE"],
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     credentials: true
-}
-app.use(cors(corsOptions))
+}))
+
 app.use(express.json())
 
 // Path for uploading files
