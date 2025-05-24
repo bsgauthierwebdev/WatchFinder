fd258da3fdc29a8e44b961a942ec3b053038fbab
Brent Gauthier <cajundrummer@gmail.com>
2025-05-23 22:27:57 -0400

    Build & style Dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import {useNavigate} from "react-router-dom"
 import {getToken} from "../utils/auth"
+import "../styles/dashboard.css"
 
 const Dashboard = () => {
   const [user, setUser] = useState(null)
