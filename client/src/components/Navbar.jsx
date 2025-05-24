fb3359a48c022702bf366d01f0999cfebb533a1b
Brent Gauthier <cajundrummer@gmail.com>
2025-05-23 22:27:08 -0400

    Build & style Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom"
 import { useAuth } from './../context/AuthContext';
+import "../styles/navbar.css"
 
 const Navbar = () => {
     const {user, logout} = useAuth()
