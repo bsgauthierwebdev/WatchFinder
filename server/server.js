ed7e18680361c9888bef60940dfa17c009044794
Brent Gauthier <cajundrummer@gmail.com>
2025-05-28 10:15:01 -0400

    Update helmet to loosen restrictions on origins

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"]
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(helmet())
+app.use(
+    helmet({
+        crossOriginResourcePolicy: {policy: "cross-origin"}
+    })
+)
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -26,6 +30,8 @@ app.use(express.json())
 
 // Path for uploading files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+// console.log("Serving uploads from: ", path.join(__dirname, "uploads"))
+
 
 // Security middleware
 app.use(securityMiddleware)
