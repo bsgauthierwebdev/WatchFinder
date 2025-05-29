31d64918fc4ce1191714ac810eb31492336efd65
Brent Gauthier <cajundrummer@gmail.com>
2025-05-28 22:58:51 -0400

    Move background-color and add logo to background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,17 @@ import Favorites from "./pages/Favorites"
 import MatchedResults from "./pages/MatchedResults"
 import Navbar from "./components/Navbar/Navbar"
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute"
+import backgroundImg from "./assets/background.png"
 import "./App.css"
 
 const App = () => {
   return (
     <div className = "main-content">
+      <img 
+        src = {backgroundImg}
+        alt = "logo"
+        className = "background-img" 
+      />
       <Router>
         <Navbar />
         <Routes>
