8c453457f7e431e9d51a8b320ff49b4784d526e9
Brent Gauthier <cajundrummer@gmail.com>
2025-06-15 11:10:31 -0400

    Build Registration page

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,85 @@
+import { useState } from "react"
+import {useNavigate} from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
 import "./register.css"
 
 const Register = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+  const {register} = useAuth()
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError("")
+
+    if (form.password !== form.confirmPassword) {
+      setError("Passwords do not match")
+      return
+    }
+
+    try {
+      console.log("Form data being submitted: ", form)
+      await register(form)
+      navigate("/dashboard")
+    } catch (err) {
+      console.error("Registration error: ", err)
+      setError(err.response?.data?.error || "Registration failed")
+    }
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register-container">
+      <form className="register-form" onSubmit = {handleSubmit}>
+        <div className="register-title">
+          <h2 className="title-text">Create Your Account</h2>
+        </div>
+        <div className="register-inputs">
+          <input 
+            type = "text"
+            name = "username"
+            placeholder = "Username"
+            onChange = {handleChange}
+            required
+          />
+          <input 
+            type = "email"
+            name = "email"
+            placeholder = "Email"
+            onChange = {handleChange}
+            required
+          />
+          <input 
+            type = "password"
+            name = "password"
+            placeholder = "Password"
+            onChange = {handleChange}
+            required
+          />
+          <input 
+            type = "password"
+            name = "confirmPassword"
+            placeholder = "Confirm Password"
+            onChange = {handleChange}
+            required
+          />
+        </div>
+        <div className="register-errors">
+          {error && <p className = "error">{error}</p>}
+        </div>
+        <div className="register-submit">
+          <button className="register-btn">Register</button>
+        </div>
+      </form>
     </div>
   )
 }
