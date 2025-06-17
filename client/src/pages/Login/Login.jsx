03e883b1c5d7794a86f9818b3540089e2eda1257
Brent Gauthier <cajundrummer@gmail.com>
2025-06-16 23:04:32 -0400

    Fix layout and styling to match Register page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -25,30 +25,38 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h2 className = "login-title">Login</h2>
       <form onSubmit = {handleSubmit} className="login-form">
-        <label htmlFor = "email" className = "login-label">Email</label>
-        <input 
-          className = "login-input"
-          id = "email"
-          type="email"
-          value = {email}
-          onChange = {(e) => setEmail(e.target.value)}
-          required 
-        />
-        <label htmlFor = "password" className = "login-label">Password</label>
-        <input 
-          className = "login-input"
-          id = "password"
-          type="password"
-          value = {password}
-          onChange = {(e) => setPassword(e.target.value)}
-          required 
-        />
-
-        {error && <p className = "login-error">{error}</p>}
-
-        <button type = "submit" className = "login-btn">Log In</button>
+        <div className="login-title">
+          <h2 className = "login-title">Login</h2>
+        </div>
+        <div className="login-inputs">
+          {/* <label htmlFor = "email" className = "login-label">Email</label> */}
+          <input 
+            className = "login-input"
+            id = "email"
+            type="email"
+            value = {email}
+            placeholder = "Email Address"
+            onChange = {(e) => setEmail(e.target.value)}
+            required 
+          />
+          {/* <label htmlFor = "password" className = "login-label">Password</label> */}
+          <input 
+            className = "login-input"
+            id = "password"
+            type="password"
+            value = {password}
+            placeholder = "Password"
+            onChange = {(e) => setPassword(e.target.value)}
+            required 
+          />
+        </div>
+        <div className="login-errors">
+          {error && <p className = "error">{error}</p>}
+        </div>
+        <div className="login-submit">
+          <button type = "submit" className = "login-btn">Log In</button>
+        </div>
       </form>
       <div className="login-redirect">
         <p>Don't have an account yet? <Link to = "/register" className = "login-redirect-link">Register your account</Link></p>
