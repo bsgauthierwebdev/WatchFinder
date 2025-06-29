f539e224776917b5f5b665b2b7995b5cfa495719
Brent Gauthier <cajundrummer@gmail.com>
2025-06-29 19:33:48 -0400

    Add route to verify email address

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser} = require("../controllers/authController")
+const {registerUser, verifyEmail, loginUser} = require("../controllers/authController")
 const {registerValidation, loginValidation} = require("../middleware/authValidator")
 
 // REGISTER NEW USER
 router.post("/register", registerValidation, registerUser)
+
+// VERIFY NEW EMAIL
+router.get("/verify-email/:token", verifyEmail)
 
 // LOGIN USER
 router.post("/login", loginValidation, loginUser)
